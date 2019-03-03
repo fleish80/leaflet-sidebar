a0dcf696822a409d880c9c4de4539e5ba4fbaf17
Dmitry Fleish <DXF0530@corp.aeroscout.com>
2019-03-03 16:46:02 +0200

    change file names and add type script support

---
+++ index.d.ts
@@ -0,0 +1,24 @@
+import * as L from 'leaflet';
+
+declare module 'leaflet' {
+
+    namespace Control {
+        type SidebarPosition = 'right' | 'left';
+
+        interface SidebarOptions {
+            closeButton?: boolean;
+            position?: SidebarPosition;
+            autoPan?: boolean;
+        }
+
+        class SidebarControl extends L.Evented {
+            constructor()
+            toggle(): void;
+        }
+
+    }
+
+    namespace control {
+        export function sidebar(placeholder: string, options?: Control.SidebarOptions): Control.SidebarControl;
+    }
+}
